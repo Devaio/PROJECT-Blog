8238c089d1fa58424095a16403ddd138a3cd9149
Devaio <robert.camp@outlook.com>
2017-03-01 12:15:03 -0700

    sync

--- app/lib/models/tags.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var mongoose_1 = require("mongoose");
-var TagSchema = new mongoose_1.Schema({
-    name: String,
-});
-exports.Tag = mongoose_1.model("Tag", TagSchema);
-//# sourceMappingURL=tags.js.map
